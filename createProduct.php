89b853b78bde62c7b3d286813cb63f459e2df8c6
Isomorfi <77666886+Isomorfi@users.noreply.github.com>
2021-11-25 14:35:52 +0100

    Add files via upload

--- createProduct.php
+++ createProduct.php
@@ -13,25 +13,68 @@ if(isset($_SESSION['signedin']) && $_SESSION['signedin'] == true && $_SESSION['u
 }
 
 
+if(isset($_GET['ProductID'])){
+    $_SESSION['ProductID'] = $_GET['ProductID'];
+    $prodid = $_SESSION['ProductID'];
+    $sql10 = "SELECT * FROM Products WHERE ProductID='$prodid'";
+    $res10 = mysqli_query($conn, $sql10);
+    $data10 = mysqli_fetch_assoc($res10);
+    
+    $name = $data10['ProductName'];
+    $price = $data10['Price'];
+	$url = $data10['PicSrc'];
+	$info = $data10['Description'];
+	$stock = $data10['Stock'];
+	$unit = $data10['Unit'];
+
+
+}
+else {
+	$prodid = 0;
+	$name = '';
+	$price = '';
+	$url = '';
+	$info = '';
+	$stock = '';
+	$unit = '';
+}
+
+
 if($_SERVER['REQUEST_METHOD'] == "POST") {
 	if(isset($_POST['create'])){
 		$name = $_POST['productname']; 
 		$price = $_POST['price'];
 		$url = $_POST['url'];
 		$info = $_POST['info'];
-                 $unit = $_POST['pricetype'];
-		$stock = $_POST['stock'];
-		if(strlen($name) > 0 && strlen($price) > 0 && strlen($url) > 0 && strlen($info) > 0 && strlen($stock) >= 0) {
-			
-
-			$sql = "INSERT INTO Products (ProductName, Price, PicSrc, Stock, Description, Unit) VALUES ('$name', '$price', '$url', '$stock', '$info', '$unit')";
-			if ($conn->query($sql) === TRUE) {
-				echo '<script>alert("Ny produkt skapad!")</script>';
-			}
-                        else {
-                            echo '<script>alert("Produkt kan inte skapas just nu! Kontakta IT-avdelningen!")</script>';
-                        }
+		if(isset($_POST['pricetype'])) {
+                		$unit = $_POST['pricetype'];
 		}
+		else {
+			$unit = '';
+		}
+		$stock = $_POST['stock'];
+		
+		if(strlen($name) > 0 && strlen($price) > 0 && strlen($url) > 0 && strlen($info) > 0 && strlen($stock) >= 0 && strlen($unit) > 0) {
+			
+			if($prodid == 0) {
+				$sql = "INSERT INTO Products (ProductName, Price, PicSrc, Stock, Description, Unit) VALUES ('$name', '$price', '$url', '$stock', '$info', '$unit')";
+				if ($conn->query($sql) === TRUE) {
+					echo '<script>alert("Ny produkt skapad!")</script>';
+					}
+                        		else {
+                            		echo '<script>alert("Produkt kan inte skapas just nu! Kontakta IT-avdelningen!")</script>';
+                        		}
+			}
+			else {
+				$sql = "UPDATE Products SET Productname='$name', Price='$price', PicSrc='$url', Stock='$stock', Description='$info', Unit='$unit' WHERE ProductID='$prodid'";	
+				if ($conn->query($sql) === TRUE) {
+					echo '<script>alert("Produkt uppdaterad!")</script>';
+				}
+                        	else {
+                                    echo '<script>alert("Produkt kan inte uppdateras just nu! Kontakta IT-avdelningen!")</script>';
+                        	}
+			}
+                }
 		else {
 			echo '<script>alert("Fält kan inte lämnas tomma!")</script>';
 		}
@@ -53,7 +96,7 @@ p    {color: #020764;}
 </head>
 <body>
 
-<h1>Guld och silver AB - Skapa ny produkt</h1>
+<h1>Guld och silver AB - Skapa/uppdatera produkt</h1>
 
 <a href="home.php"><button type="submit" value="Submit">Logga ut</button></a>
 
@@ -74,7 +117,7 @@ p    {color: #020764;}
     <fieldset class="fieldset-auto-width">
 <form name="form" method="POST">
 
-<div style="width:450px;">
+<div style="width:500px;">
   <div style="width:150px; float:left;">
 <p><label for="productname">Produktnamn:</label></p><br>
 <p><label for="price">Pris:</label></p><br>
@@ -82,30 +125,27 @@ p    {color: #020764;}
 <p><label for="url">Lagersaldo:</label></p><br>
 <p><label for="info">Produktinfo:</label></p>
 </div>
-  <div style="width:250px; float:left;">
-<p><input type="text" id="productname" name="productname"></p><br>
-<p><input type="text" id="price" name="price"><label for="pricetype"></label>
+  <div style="width:350px; float:left;">
+<p><input type="text" id="productname" value="<?php echo $name?>" name="productname"></p><br>
+<p><input type="text" id="price" value="<?php echo $price?>" name="price"><label for="pricetype"></label>
+
   <select name="pricetype" id="pricetype">
+    <option value="" selected disabled hidden>Välj enheten</option>
     <option value="gram">gram</option>
     <option value="kg">kg</option>
     <option value="st">st</option>
-  </select>
+</select>
   <br></p><br>
-<p><input type="url" id="text" name="url"><label></p><br>
-<p><input type="text" id="text" name="stock"><label></p><br>
-<p><textarea name="info" cols="40" rows="5"></textarea></p>
+<p><input type="url" id="text" value="<?php echo $url?>" name="url"><label></p><br>
+<p><input type="text" id="text" value="<?php echo $stock?>" name="stock"><label></p><br>
+<p><textarea name="info" cols="40" rows="5"><?php echo $info?></textarea></p>
 </div>
 </div>
 <div style="clear: both;"></div>
 
 
 
-
-
-
-
-
-       <p style="text-align:center;"><button type="submit" name="create" value="Submit">Skapa</button></p>
+       <p style="text-align:center;"><button type="submit" name="create" value="Submit">Skicka</button></p>
      </form></fieldset>
 </fieldset>
 
